af6711acdc35e0042d7dbebf0d95759c157bb956
nathenjo <nateselectronicrepair@gmail.com>
2023-05-15 22:36:15 -0400

    New calendar date changing functionality is working

--- components/newCalendar.js
+++ components/newCalendar.js
@@ -4,24 +4,43 @@ export default function NewCalendar(props) {
 
     const {} = props;
 
-    const startDate = new Date();
-
-    const [curMonth, setCurMonth] = useState(startDate);
+    const [curDate, setCurDate] = useState(new Date());
+    const [dateChange, setDateChange] = useState('month');
 
     useEffect(() => {
-        console.log('Calendar Component mounted')
-    }, [])
+        
+    }, []);
 
+    useEffect(() => {
+        
+    }, [curDate])
+
+    const changeDate = (op) => {
+        let tempDate = new Date(curDate);
+        if (op == 'increase') {
+            if (dateChange == 'month') {
+                setCurDate(new Date(tempDate.getFullYear(),tempDate.getMonth() + 1, tempDate.getDate()));
+            } else if (dateChange == 'year') {
+                setCurDate(new Date(tempDate.getFullYear() + 1,tempDate.getMonth(), tempDate.getDate()));
+            }
+        } else if (op == 'decrease') {
+            if (dateChange == 'month') {
+                setCurDate(new Date(tempDate.getFullYear(),tempDate.getMonth() - 1, tempDate.getDate()));
+            } else if (dateChange == 'year') {
+                setCurDate(new Date(tempDate.getFullYear() - 1,tempDate.getMonth(), tempDate.getDate()));
+            }
+        }
+    }
 
     return (
         <div className='new-cal'>
             <div className='new-cal__header'>
-                <button>Previous Month</button>
+                <button onClick={() => changeDate('decrease')}>Previous Month</button>
                 <div className='current-date-wrapper'>
-                    <span className='current-month'>{curMonth.toLocaleString('en-us',{month:'long'})}</span>
-                    <span className='current-year'>Current Year</span>
+                    <span onClick={() => setDateChange('month')} className='current-month'>{curDate.toLocaleString('en-us',{month:'long'})}</span>
+                    <span onClick={() => setDateChange('year')} className='current-year'>{curDate.toLocaleString('en-us',{year:'numeric'})}</span>
                 </div>
-                <button>Next Month</button>
+                <button onClick={() => changeDate('increase')}>Next Month</button>
             </div>
             <div className='new-cal__main'>
 
