f780fced340c9ce0e7c6f7753ac31d31f9085e68
nathenjo <nateselectronicrepair@gmail.com>
2023-05-27 21:41:56 -0400

    Reworked input component to take attributes array instead of individual props for attributes

--- components/input.js
+++ components/input.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Input(props) {
 
-    const {propClass, inputType, propLength, propPlace} = props;
+    const {propClass, propAttr} = props;
 
-    const formatIcon = (valStr) => {
-        return <div dangerouslySetInnerHTML={{__html: `${valStr}`}} />
-    };
+    useEffect(() => {
+        if (propAttr) {
+            propAttr.map(item => {
+                for (const attr in item) {
+                    console.log(attr, item[attr]);
+                    document.querySelector(`.${propClass}-input`).setAttribute(attr,item[attr]);
+                }
+            })
+        }
+    }, [])
 
     return (
         <div className={`comp-input ${propClass}-wrapper`}>
             <h3 className={`${propClass}-label`}>{propClass.replace('-', ' ')}</h3>
-            <input maxLength={propLength} placeholder={propPlace} type={inputType} className={`${propClass}-input`} />
+            <input className={`${propClass}-input`} />
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Calendar from '../components/calendar';
 import Input from '../components/input';
 import NewCalendar from '../components/newCalendar';
@@ -7,7 +7,34 @@ export default function Home(props) {
 
     const {} = props;
 
+    useEffect(() => {
+        console.log('Index loaded');
+        getCBData();
+    }, [])
+
+    const dummyData = [
+        {
+            'id': '4262023',
+            'income-total': '2002.71',
+            'bills-total': '1536.84'
+        },
+        {
+            'id': '5122023',
+            'income-total': '2005.71',
+            'bills-total': '1465.07'
+        }
+    ]
+
+    const getCBData = (cbid) => {
+        dummyData.map(item => {
+            if (item['id'] == cbid) {
+                console.log(item['income-total'], item['bills-total']);
+            }
+        })
+    }
+
     const [selDate, setSelDate] = useState('');
+    const [checkData, setCheckData] = useState([]);
 
     return (
         <div className='home-wrapper'>
@@ -21,12 +48,12 @@ export default function Home(props) {
                 <div className='main-column-left'>
                     <div className='existing-checkbooks-wrapper'>
                         <h2>Existing Checkbooks</h2>
-                        <select>
-                            <option>Checkbook Date 4-26-2023</option>
-                            <option>Checkbook Date 5-12-2023</option>
+                        <select onChange={(e) => console.log('Select changed', e.target)}>
+                            <option value='4262023'>Checkbook Date 4-26-2023</option>
+                            <option value='5122023' selected>Checkbook Date 5-12-2023</option>
                         </select>
                     </div>
-                    <Input propClass='income' inputType='text' propPlace='$ 0.00' />
+                    <Input propAttr={[{'name': 'Test Name'}]}propClass='income' inputType='text' propPlace='$ 0.00' />
                     <Input propClass='bills' inputType='text' propPlace='$ 0.00' />
                     <Input propClass='leftover' inputType='text' propPlace='$ 0.00' />
                     <Input propClass='add-checkbook' inputType='text' propPlace='Checkbook Name...' />
